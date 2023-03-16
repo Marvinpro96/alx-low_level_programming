66071089baf8a8ba213b589e27d04d9e5313910a
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-16 16:21:25 -0700

    a C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,6 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const int msg_len = sizeof(msg) - 1; /* exclude null terminator*/
-	write(STDERR_FILENO, msg, msg_len);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
