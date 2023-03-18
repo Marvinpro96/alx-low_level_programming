5c74a7b03c77efd0b34482c97d6240500b002000
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-18 12:58:55 -0700

    If else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,19 +5,20 @@
 
 /* betty style doc for function main goes there */
 int main(void)
+	/*Main function*/
 {
 	int n;
 /* Seed the random number generator with the current time*/
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* Check if the number is positive */
-	if (n>0)
+	if (n > 0)
 	{
 		/*Print the number and a message indicating that it is positive*/
 		printf("%d is positive\n", n);
 		/*Check if the number is zero*/
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
 	/*Print the number and a message indicating that it is zero*/
 		printf("%d is zero \n", n);
