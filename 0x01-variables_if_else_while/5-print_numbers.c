b2cf7de30d85f013c944af20aa63b5ef8428908d
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-20 11:42:23 -0700

    for and printf

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@ int main(void)
 	int single_num;
 
 	/* Prints all single digit numbers of base 10 starting from 0.*/
-	for (single_num = 0; single_num <= 10; single_num++)
+	for (single_num = 0; single_num < 10; single_num++)
 	{
 		printf("%d", single_num);
 	}
