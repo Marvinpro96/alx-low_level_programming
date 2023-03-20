4d320cb8b4e0ee5f94df99c43bca9562f0138ce9
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-20 09:00:37 -0700

    for statement

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	char ch;
+	/*print lowercase alphabet using putchar*/
+	for (ch = 'a'; ch <= 'z'; ch++) 
+	{
+		putchar(ch);
+	}
+	/*print a new line using putchar*/
+
+	putchar('\n');
+
+	return 0;
+}
