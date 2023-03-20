38212de42649f46b3411325e492d372890fc535b
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-20 11:56:02 -0700

    all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int single_num;
+
+	for (single_num =0; single_num < 10; single_num++;)
+	{
+		putchar(single_num);
+	}
+	putchar("\n");
+	return (0);
+}
