950987239cb6a5ff8cd6758a1c1cb8a624940556
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-27 12:48:20 -0700

    a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
+	int i;
 	char password[11]; /* allocate space for password (10 characters + null terminator)*/
 	srand(time(NULL)); /*seed random number generator with current time*/
 	/* generate random characters for password*/
-	int i;
-	for (int i = 0; i < 10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
 		password[i] = rand() % 94 + 33; /* ASCII characters 33-126 are valid password characters*/
 	}
