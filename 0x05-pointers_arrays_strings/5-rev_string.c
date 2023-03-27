aa0da586c7477aef98b4806676aea5a1ce54a0ec
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-27 08:08:20 -0700

    a function that reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,26 +7,29 @@
   */
 void rev_string(char *s)
 {
-	int i, r, k;
+	if (s == NULL)
+	{
+		return;
+	}
+	int i, r = 0, k;
 	char *a, mav;
-
+	
 	a = s;
-
 	while (s[r] != '\0')
 	{
 		r++;
 	}
-
 	for (k = 1; k < r; k++)
 	{
 		a++;
 	}
-
 	for (i = 0; i < (r / 2); i++)
 	{
-		mav = s[i];
-		s[i] = *a;
-		*a = mav;
-		a--;
+		if (&s[i] != &s[r - 1 - i])
+		{
+			mav = s[i];
+			s[i] = s[r - 1 - i];
+			s[r - 1 - i] = mav;
+		}
 	}
 }
