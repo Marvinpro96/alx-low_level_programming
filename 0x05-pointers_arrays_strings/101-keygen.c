939ecbf5304c9afc416e8041729f302a209758e6
Marvinpro96 <marvinonyango96@gmail.com>
2023-03-27 12:26:37 -0700

    a program that generates random valid passwords for the program 101-crackme.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	srand(time(NULL)); /*seed random number generator with current time*/
+	char password[11]; // allocate space for password (10 characters + null terminator) 
+	/* generate random characters for password*/
+	for (int i = 0; i < 10; i++)
+	{
+		password[i] = rand() % 94 + 33; /* ASCII characters 33-126 are valid password characters*/
+	}
+	password[10] = '\0'; /* null terminate the password string*/
+	printf("%s\n", password); /* print the password to the console*/
+	return (0);
+}
